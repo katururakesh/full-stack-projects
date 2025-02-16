6de8b8d7944677ec7ec80d018b3ff3e8e657a8e7
katururaksh@gmail.com <katururaskesh>
2025-02-17 01:46:39 +0530

    initial commit

---
+++ chat-applictaion/src/main/java/com/chat/ChatApplictaionApplication.java
@@ -0,0 +1,15 @@
+package com.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ChatApplictaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplictaionApplication.class, args);
+	}
+
+}
